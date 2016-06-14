871fedd0687d556d1c4f8b5a641883937573875d
andrijadjordjevic1995 <andrija.djordjevic1995@gmail.com>
2016-06-14 21:00:11 +0200

    Dodata metoda za predvidjanje narednih n karaktera na osnovu ulaznog stringa

--- CollectionsHomework/src/paket/Bigram.java
+++ CollectionsHomework/src/paket/Bigram.java
@@ -2,6 +2,9 @@ package paket;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Map.Entry;
 
 public class Bigram {
@@ -15,14 +18,15 @@ public class Bigram {
 		for (int i = 0; i < ulazni.length() - 1; i++) {
 			bigram = bigram + ulazni.charAt(i) + ulazni.charAt(i + 1);
 			if (frekvencije.containsKey(bigram)) {
-				frekvencije.replace(bigram, frekvencije.get(bigram) + 1);// povecaj
-																			// frekvenciju
+				frekvencije.replace(bigram, frekvencije.get(bigram) + 1);// povecaj frekvenciju
 			} else {
 				frekvencije.put(bigram, 1);
 			}
 			bigram = "";
 		}
-		Bigram.ispisiFrekvencije((HashMap<String, Integer>)frekvencije);
+		Bigram.ispisiFrekvencije((HashMap<String, Integer>) frekvencije);
+		Bigram.predvidiNarednihNKaraktera((HashMap<String, Integer>) frekvencije, 3);
+		
 		
 	}
 
@@ -31,5 +35,32 @@ public class Bigram {
 		for (Entry<String, Integer> entry : frekvencije.entrySet()) {
 			System.out.println(entry.getKey()+": "+entry.getValue());
 		}
+	}
+	
+	public static void predvidiNarednihNKaraktera(HashMap<String, Integer> frekvencije, int n){
+		Scanner scanner = new Scanner(System.in);
+		Pattern regex;
+//		Matcher matcher;
+		
+		System.out.println("Unesite string: ");
+		String ulazIzlaz = scanner.nextLine();
+		int maxFrek;//nema potrebe izracunavati verovatnoce,
+					//dovoljno je uporediti frekvencije pojavljivanja 
+					//svih bigrama koji pocinju karakterom kojim se zavrsava string ulazIzlaz
+		char c;
+		
+		while(n-- != 0){
+			regex = Pattern.compile(ulazIzlaz.charAt(ulazIzlaz.length()-1)+"[A-Za-z]");
+			maxFrek = 0;
+			c = ' ';
+			for (Entry<String, Integer> entry : frekvencije.entrySet()) {
+				if(regex.matcher(entry.getKey()).matches() && entry.getValue() > maxFrek){
+					maxFrek = entry.getValue();
+					c = entry.getKey().charAt(1);
+				}
+			}
+			ulazIzlaz += c;
+		}
+		System.out.println(ulazIzlaz);
 	}
 }
